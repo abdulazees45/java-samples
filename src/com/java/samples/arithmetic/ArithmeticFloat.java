1879976e8b83eb0c35aa351b6cae47ebb43a575b
abdul azees <azeesrahim1200@gmail.com>
2022-10-07 21:53:16 -0700

    modified to add comment

--- src/com/java/samples/arithmetic/ArithmeticFloat.java
+++ src/com/java/samples/arithmetic/ArithmeticFloat.java
@@ -13,7 +13,7 @@ public class ArithmeticFloat {
 		Scanner inputFloat = new Scanner(System.in);
 		System.out.println("Enter the float value for a1 & b1");
 		a1 = inputFloat.nextFloat();
-		b1 = inputFloat.nextFloat();
+		b1 = inputFloat.nextFloat();//convert from string to float
 
 		Arithmetic a = new Arithmetic();
 		float c1 = a.add(a1, b1);
